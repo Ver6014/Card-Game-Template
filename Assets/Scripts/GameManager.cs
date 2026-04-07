3cc690e9be8810f4512d75bd56320ac4f909cf79
Ver6014 <v.schulhoff@gmail.com>
2026-04-07 14:58:01 -0700

    clone

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager gm;
-    public List<Card> deck = new List<Card>();
-    public List<Card> player_deck = new List<Card>();
-    public List<Card> ai_deck = new List<Card>();
-    public List<Card> player_hand = new List<Card>();
-    public List<Card> ai_hand = new List<Card>();
-    public List<Card> discard_pile = new List<Card>();
+    public List<Card_data> deck = new List<Card_data>();
+    public List<Card_data> player_deck = new List<Card_data>();
+    public List<Card_data> ai_deck = new List<Card_data>();
+    public List<Card_data> player_hand = new List<Card_data>();
+    public List<Card_data> ai_hand = new List<Card_data>();
+    public List<Card_data> discard_pile = new List<Card_data>();
+
+    public Canvas canvas;
+    public Vector3 player_hand_pos;
+    public Card blank;
 
     private void Awake()
     {
@@ -38,6 +42,24 @@ public class GameManager : MonoBehaviour
 
     void Deal()
     {
+        for (int i = 0; i < 5; i++)
+        {
+            Card top_card = Instantiate(blank, player_hand_pos, Quaternion.identity, canvas.transform);
+
+            player_hand_pos.x += 300;
+
+            top_card.data = player_deck[0];
+
+            player_hand.Add(top_card.data);
+
+           player_deck.RemoveAt(0);
+
+           /*
+            player_deck.RemoveAt(0);
+            ai_hand.Add(ai_deck[0]);
+            ai_deck.RemoveAt(0);
+            */
+        }
 
     }
 
